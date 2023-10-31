9b4078c7f82efd936b17b269759ef47be8f80766
neo masilo <neolawrencemasilo@gmail.com>
2023-10-31 07:02:46 +0000

    update

--- binary_trees.h
+++ binary_trees.h
@@ -1,5 +1,5 @@
-#ifndef BINARY_TREES_H
-#define BINARY_TREES_H
+#ifndef _BINARY_TREES_H_
+#define _BINARY_TREES_H_
 
 #include <stdio.h>
 #include <stdlib.h>
@@ -22,9 +22,9 @@ struct binary_tree_s
 };
 
 typedef struct binary_tree_s binary_tree_t;
+typedef struct binary_tree_s bst_t;
 
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value);
-void binary_tree_print(const binary_tree_t *);
 binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value);
 binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value);
 #endif
